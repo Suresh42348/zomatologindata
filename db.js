2f4d80c50514b36ac4dca316f23e4501e7998035
Suresh42348 <panku42348@gmail.com>
2022-04-10 18:06:07 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+const mongoLiveUrl = "mongodb+srv://Suresh:Suresh123@cluster0.mton8.mongodb.net/zommato?retryWrites=true&w=majority"
+mongoose.connect(mongoLiveUrl)
